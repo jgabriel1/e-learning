5e33682e89f78cf479cfc25d81ddc9b75e9bde15
jgabriel1 <josegabriel@det.ufc.br>
2020-12-26 16:08:10 -0300

    Created courses route handler functions.

---
+++ backend/elearning_backend/handlers/courses.py
@@ -0,0 +1,61 @@
+from typing import Optional
+from fastapi import APIRouter, Depends, Header
+from pydantic import BaseModel
+
+from ..database.repositories import CoursesRepository, LessonsRepository
+
+courses_router = APIRouter(prefix="/courses")
+
+
+class CreateNewCourseData(BaseModel):
+    name: str
+    image: str
+
+
+class UpdateCourseData(BaseModel):
+    name: str = None
+    image: str = None
+
+
+@courses_router.get("/")
+async def list_all_courses(courses_repository: CoursesRepository = Depends()):
+    courses = await courses_repository.findAll()
+
+    return courses
+
+
+@courses_router.get("/{course_id}/lessons")
+async def list_lessons_for_course(
+    course_id: int,
+    mac_id: Optional[str] = Header(None),
+    lessons_repository: LessonsRepository = Depends(),
+):
+    lessons = await lessons_repository.list_by_course_id(course_id)
+
+    return lessons
+
+
+@courses_router.post("/")
+async def create_course(
+    new_course: CreateNewCourseData,
+    courses_repository: CoursesRepository = Depends(),
+):
+    created_course = await courses_repository.create(
+        name=new_course.name,
+        image_url=new_course.image,
+    )
+
+    return created_course
+
+
+@courses_router.put("/:course_id", status_code=204)
+async def update_course(
+    course_id: int,
+    update_course_data: UpdateCourseData,
+    courses_repository: CoursesRepository = Depends(),
+):
+    await courses_repository.update_by_id(
+        course_id,
+        name=update_course_data.name,
+        image=update_course_data.image,
+    )
