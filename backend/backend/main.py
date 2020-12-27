f6aac12bccac5f3763b817aeedb9a29736fa23a0
jgabriel1 <josegabriel@det.ufc.br>
2020-12-27 05:31:30 -0300

    Moved app logic to a separate class.

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,26 +1,3 @@
-from fastapi import FastAPI
+from backend.shared.app import App
 
-from backend.modules.courses.handlers import courses_router
-from backend.modules.lessons.handlers import lessons_router
-
-from .shared.database.connection import get_database_connection
-
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def startup():
-    database = get_database_connection()
-
-    database.create_tables()
-    await database.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    database = get_database_connection()
-    await database.disconnect()
-
-
-app.include_router(courses_router)
-app.include_router(lessons_router)
+app = App().get_app()
