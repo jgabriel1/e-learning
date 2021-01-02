a10a0b16732256878fd4ce0b02bc10d5068acc55
jgabriel1 <josegabriel@det.ufc.br>
2021-01-02 19:49:36 -0300

    Added fonts loading to index.

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Rubik_400Regular } from '@expo-google-fonts/rubik';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 
 import Home from './pages/Home';
 
+// rubik 400 roboto 400 500
+
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    rubik400: Rubik_400Regular,
+    roboto400: Roboto_400Regular,
+    roboto500: Roboto_500Medium,
+  });
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <StatusBar style="light" />
-      <Home />
+      {fontsLoaded ? (
+        <>
+          <StatusBar style="light" />
+          <Home />
+        </>
+      ) : (
+        <ActivityIndicator />
+      )}
     </View>
   );
 };
