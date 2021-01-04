3ab9cef217d62aacdcad5224fe0b953365b1aa71
jgabriel1 <josegabriel@det.ufc.br>
2021-01-03 22:48:45 -0300

    Added custom hooks to access entities ids.

--- mobile/src/routes/lessons.tsx
+++ mobile/src/routes/lessons.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Lessons from '../pages/Lessons';
 import LessonDetail from '../pages/LessonDetail';
 
-const { Navigator, Screen } = createStackNavigator();
+type LessonsStackParams = {
+  Lessons: { course_id: number };
+  LessonDetail: { lesson_id: number };
+};
+
+type LessonsRouteProp<T extends 'Lessons' | 'LessonDetail'> = RouteProp<
+  LessonsStackParams,
+  T
+>;
+
+const { Navigator, Screen } = createStackNavigator<LessonsStackParams>();
 
 const LessonsStack: React.FC = () => (
   <Navigator headerMode="none">
@@ -14,3 +25,15 @@ const LessonsStack: React.FC = () => (
 );
 
 export default LessonsStack;
+
+export function useCourseId() {
+  const { params } = useRoute<LessonsRouteProp<'Lessons'>>();
+
+  return params.course_id;
+}
+
+export function useLessonId() {
+  const { params } = useRoute<LessonsRouteProp<'LessonDetail'>>();
+
+  return params.lesson_id;
+}
