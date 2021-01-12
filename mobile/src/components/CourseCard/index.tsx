91a24bae9a4dde38127ea6b42e1d98d978145ee9
jgabriel1 <josegabriel@det.ufc.br>
2021-01-11 23:48:10 -0300

    Called the courses hook to present data on the home page.

--- mobile/src/components/CourseCard/index.tsx
+++ mobile/src/components/CourseCard/index.tsx
@@ -9,6 +9,12 @@ import {
   CourseImage,
 } from './styles';
 
+/**
+ * TODO:
+ * Modify items to be only one per row. Since course names are too long, it gets
+ * too cluttered to have two courses per row.
+ */
+
 interface CourseCardProps {
   title: string;
   lessonsCount: number;
