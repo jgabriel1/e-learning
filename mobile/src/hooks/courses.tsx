5305cda4e2ef22eb23130a5a5d8239a559a97a11
jgabriel1 <josegabriel@det.ufc.br>
2021-01-11 23:44:23 -0300

    Created hook for fetching courses data from the backend.

---
+++ mobile/src/hooks/courses.tsx
@@ -0,0 +1,59 @@
+import React, { createContext, useContext } from 'react';
+import useSWR from 'swr';
+
+import api from '../services/api';
+
+/**
+ * BACKEND TODO:
+ * Change on the backend the names of the properties to match the client;
+ * Add total lessons count to the service;
+ */
+
+interface ICourseResponseData {
+  id: number;
+  name: string;
+  image: string;
+}
+
+interface ICourseData {
+  title: string;
+  lessonsCount: number;
+  imageURL: string;
+}
+
+interface CoursesContextData {
+  courses: ICourseData[];
+}
+
+const CoursesContext = createContext<CoursesContextData>(
+  {} as CoursesContextData,
+);
+
+export const CoursesProvider: React.FC = ({ children }) => {
+  const { data: courses } = useSWR<ICourseData[]>(
+    'courses',
+    async (url: string) => {
+      const response = await api.get<ICourseResponseData[]>(url);
+
+      return response.data.map(course => ({
+        title: course.name,
+        imageURL: course.image,
+        lessonsCount: 0,
+      }));
+    },
+  );
+
+  return (
+    <CoursesContext.Provider
+      value={{
+        courses: courses || [],
+      }}
+    >
+      {children}
+    </CoursesContext.Provider>
+  );
+};
+
+export function useCourses() {
+  return useContext(CoursesContext);
+}
