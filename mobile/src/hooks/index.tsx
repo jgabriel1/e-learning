eda284c6ec2127b5895c09eda94da0b7387796ee
jgabriel1 <josegabriel@det.ufc.br>
2021-01-14 01:21:15 -0300

    Applied lessons provider to the app.

--- mobile/src/hooks/index.tsx
+++ mobile/src/hooks/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import { CoursesProvider } from './courses';
+import { LessonsProvider } from './lessons';
 
 const AppProvider: React.FC = ({ children }) => {
-  return <CoursesProvider>{children}</CoursesProvider>;
+  return (
+    <CoursesProvider>
+      <LessonsProvider>{children}</LessonsProvider>
+    </CoursesProvider>
+  );
 };
 
 export default AppProvider;
