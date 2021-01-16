c4663a738e40e9ce410a97acb1a4856b9ac2bdc7
jgabriel1 <josegabriel@det.ufc.br>
2021-01-16 19:10:47 -0300

    Added video player to the routes.

--- mobile/src/routes/lessons.tsx
+++ mobile/src/routes/lessons.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Lessons from '../pages/Lessons';
 import LessonDetail from '../pages/LessonDetail';
+import PlayVideo from '../pages/PlayVideo';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -10,6 +11,7 @@ const LessonsStack: React.FC = () => (
   <Navigator headerMode="none">
     <Screen name="Lessons" component={Lessons} />
     <Screen name="LessonDetail" component={LessonDetail} />
+    <Screen name="PlayVideo" component={PlayVideo} />
   </Navigator>
 );
 
