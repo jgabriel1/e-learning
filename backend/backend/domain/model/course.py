eeffa81c229f9d07de9c312cc04007902b14dc18
jgabriel1 <josegabriel@det.ufc.br>
2021-02-04 06:41:50 -0300

    Altered course model to parse lessons quantity when lessons prop is loaded.

--- backend/backend/domain/model/course.py
+++ backend/backend/domain/model/course.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import AnyHttpUrl
+from pydantic import AnyHttpUrl, validator
 
 from backend.domain.model.base import Base
 from backend.domain.model.lesson import Lesson
@@ -9,4 +9,11 @@ from backend.domain.model.lesson import Lesson
 class Course(Base):
     name: str
     image: AnyHttpUrl
+    lessons_count: int = None
     lessons: List[Lesson] = None
+
+    @validator("lessons_count", pre=True)
+    def get_lessons_array_length(cls, _, values):
+        lessons = values["lessons"]
+        if lessons is not None:
+            return len(lessons)
