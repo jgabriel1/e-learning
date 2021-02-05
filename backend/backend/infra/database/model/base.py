90869c827104188fc55ce0288bd76aeb01bd4b9f
jgabriel1 <josegabriel@det.ufc.br>
2021-02-05 15:39:01 -0300

    Changed the validation approach for connection between infra and domain models.

--- backend/backend/infra/database/model/base.py
+++ backend/backend/infra/database/model/base.py
@@ -1,21 +1,46 @@
+from typing import Type
+
 from sqlalchemy import Column, DateTime, Integer, func, MetaData
 from sqlalchemy.ext.declarative import as_declarative
 
-from backend.domain.exception import DomainValidationError
+from backend.domain.model.base import Base as DomainBaseModel
 
 
 @as_declarative(metadata=MetaData())
 class DeclarativeBase:
+    __domain__: Type[DomainBaseModel] = None
+
+    _model: DomainBaseModel
+
     metadata: MetaData
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-    def __new__(cls, *args, **kwargs):
-        if hasattr(cls, "__domain__"):
-            cls.__domain__(*args, **kwargs)
-        else:
-            raise DomainValidationError("Must have a model to validate in __domain__.")
+    def __init__(self, *args, **kwargs):
+        ...
 
-        return super().__new__(cls)
+    @classmethod
+    def from_domain(cls, entity: DomainBaseModel) -> "DeclarativeBase":
+        if type(entity) != cls.__domain__:
+            raise TypeError("Entity must have same declared model.")
+
+        values = {
+            key: value
+            for key, value in entity
+            if hasattr(cls, key) and value is not None
+        }
+
+        instance = cls(**values)
+
+        instance._model = entity
+
+        return instance
+
+    def save_model(self) -> None:
+        for key, value in self._model:
+            saved_value = getattr(self, key, None)
+
+            if saved_value != value:
+                setattr(self._model, key, saved_value)
