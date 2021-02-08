ccd0f9e836c2845de080f175d0b21040a2a9f6f4
jgabriel1 <josegabriel@det.ufc.br>
2021-02-08 15:11:42 -0300

    Update tests for database mapping validation.

--- backend/backend/infra/database/model/__tests__/test_base_mapping_validation.py
+++ backend/backend/infra/database/model/__tests__/test_base_mapping_validation.py
@@ -3,7 +3,6 @@ from unittest import TestCase
 from pydantic import AnyHttpUrl
 from sqlalchemy import Column, String, Integer
 
-from backend.domain.exception import DomainValidationError
 from backend.domain.model.base import Base as DomainBase
 from backend.infra.database.model.base import DeclarativeBase
 
@@ -22,13 +21,30 @@ class TestBaseMappingValidation(TestCase):
             prop1 = Column(String, nullable=False)
             prop2 = Column(Integer, nullable=True)
 
-        cls.Model = TestModel
+        cls.DomainModel = TestDomainModel
+        cls.DBModel = TestModel
 
-    def test_domain_model_validation(self):
-        instance = self.Model(prop1="http://test.com/test", prop2=123)
+    def test_domain_model_parsing(self):
+        domain_instance = self.DomainModel.parse_obj(
+            {"prop1": "http://test.com/test", "prop2": 123}
+        )
 
-        self.assertIsInstance(instance, self.Model)
+        db_instance = self.DBModel.from_domain(domain_instance)
 
-    def test_instantiate_with_wrong_types(self):
-        with self.assertRaises(DomainValidationError):
-            self.Model(prop1="invalid.url")
+        self.assertIsInstance(db_instance, self.DBModel)
+        self.assertEqual(db_instance.prop1, domain_instance.prop1)
+        self.assertEqual(db_instance.prop2, domain_instance.prop2)
+
+    def test_domain_model_saving(self):
+        domain_instance = self.DomainModel.parse_obj(
+            {"prop1": "http://test.com/test", "prop2": 123}
+        )
+
+        db_instance = self.DBModel.from_domain(domain_instance)
+
+        db_instance.prop2 = 456
+
+        db_instance.save_model()
+
+        self.assertIs(domain_instance, db_instance._model)
+        self.assertEqual(domain_instance.prop2, 456)
