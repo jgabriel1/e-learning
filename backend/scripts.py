51e4aea47218b16d977897b64616d03cb2831c8c
jgabriel1 <josegabriel@det.ufc.br>
2021-02-11 16:44:10 -0300

    Added production script.

--- backend/scripts.py
+++ backend/scripts.py
@@ -1,8 +1,20 @@
+import os
+
 import uvicorn
 
 
 def dev():
+    os.environ["ENV"] = "DEV"
+
     uvicorn.run(
         app="backend.application.app:app",
         reload=True,
+    )
+
+
+def prod():
+    os.environ["ENV"] = "PROD"
+
+    uvicorn.run(
+        app="backend.application.app:app",
     )
